1702768b18f76a5d0046e841486e42be7997405b
XLsn0w <xlsn00w@gmail.com>
2021-02-04 14:44:40 +0800

    init

---
+++ AltStore/AltServer/AltServer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ALTDeviceManager.h"
+#import "ALTWiredConnection.h"
+#import "ALTNotificationConnection.h"
+
+// Shared
+#import "ALTConstants.h"
+#import "ALTConnection.h"
+#import "NSError+ALTServerError.h"
+#import "CFNotificationName+AltStore.h"
